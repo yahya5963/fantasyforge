347ca1751491f1138da83faa0cf7f9f0a3021d59
yahya5963 <yahyakherbek9@gmail.com>
2026-06-17 09:17:02 +0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const NAME_DATA = {
+    kingdom: {
+        pre: ["Aether", "Storm", "Iron", "Gold", "Shadow", "Frost", "Dread", "Ember", "Star", "Mist", "Obsidian", "Raven", "Azure", "Crimson", "Veridian", "Onyx", "High", "North", "South", "East", "West"],
+        suf: ["gard", "ia", "vale", "spire", "hold", "reach", "rock", "fall", "gate", "bridge", "shire", "land", "gard", "thorp", "burg", "port", "stead", "field", "crest"]
+    },
+    dragon: {
+        pre: ["Bal", "Ig", "Valth", "Drak", "Pyr", "Gla", "Saph", "Obsid", "Vern", "Zor", "Ignis", "Glac", "Terr", "Aether", "Vor", "Mal", "Kha", "Zul", "Rha", "Gorth"],
+        suf: ["rax", "un", "ax", "os", "ith", "on", "ar", "ur", "eth", "og", "thar", "ion", "is", "th", "or", "ul", "yx", "en", "oth", "am"]
+    },
+    // ... add data for all 10 tools here ...
+};
